35399b6ef35bcdd36ebf54ff1849edba0bc9e2cd
lele416 <83065657+lele416@users.noreply.github.com>
2021-11-18 15:52:53 +0800

    Add files via upload

---
+++ ProyekPW/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Login</title>
+  <link rel="stylesheet" href="asset/css/login.css">
+</head>
+
+<body>
+  <div class="background">
+
+  </div>
+  <form>
+    <h3>Login Here</h3>
+
+    <label for="username">Username</label>
+    <input type="text" placeholder="Email or Phone" id="username">
+
+    <label for="password">Password</label>
+    <input type="password" placeholder="Password" id="password">
+
+    <button class="alo">Log In</button>
+    <div class="social">
+      <a href="index.php" style="text-decoration: none"><div class="back">Back</div></a>
+      <a href="register.php" style="text-decoration: none"><div class="reg">Register</div></a>
+    </div>
+  </form>
+</body>
+
+</html>
