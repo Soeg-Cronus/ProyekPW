98f0626a17aca4c2330b5d52583cad2338a85983
Soeg-Cronus <83352641+Soeg-Cronus@users.noreply.github.com>
2021-11-20 20:16:58 +0700

    register

--- register.php
+++ register.php
@@ -11,11 +11,18 @@
   <div class="background">
 
   </div>
+<div class="buatform">
   <form method="post" action="">
     <h3>Register Here</h3>
 
     <label for="username">Username</label>
     <input type="text" placeholder="Email or Phone" id="username">
+
+    <label for="datetime">Birth Date</label>
+    <input type="date" id="start" name="trip-start" min="1921-01-01" max="2002-12-31">
+
+    <label for="gender">Gender</label>
+    <input type="text" placeholder="Pria/Wanita" id="gender">
 
     <label for="password">Password</label>
     <input type="password" placeholder="Password" id="password">
@@ -23,12 +30,15 @@
     <label for="repassword">Confirm Password</label>
     <input type="password" placeholder="Confirm Password" id="repassword">
 
+
+
     <button class="alo">Register</button>
     <div class="social">
       <a href="index.php" style="text-decoration: none"><div class="back">Back</div></a>
       <a href="login.php" style="text-decoration: none"><div class="reg">Login</div></a>
     </div>
   </form>
+</div>
 </body>
 
 </html>
